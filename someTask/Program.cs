ed6974a02dc651359cf7214f232c20609f8ba68a
mixiHatter <5281cmixi@gmail.com>
2024-10-15 19:51:11 +0300

    addedNewTask
    completed

---
+++ someTask/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cospect
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // cw +2xTAB =
+            // Console.WriteLine(  );
+            // интерполяция:
+            //Console.WriteLine($" someText {someVar} someText {someVar}");
+            // Convert.To    ->
+            // Int32 (int); Single (float); String (string)
+            // Console.ReadKey(); - ожидание нажатие любой клавиши
+            // Console.ReadLine(); - ожидание ввода строки
+
+        }
+    }
+}
---
+++ work With Strings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workWithStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            string zodiacSign;
+            string profession;
+
+            Console.Write("Ваше имя: ");
+            name = Console.ReadLine();
+            Console.Write("Ваш возраст: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ваш знак зодиака: ");
+            zodiacSign = Console.ReadLine();
+            Console.Write("Ваша профессия: ");
+            profession = Console.ReadLine();
+
+            Console.WriteLine($"Вас зовут {name}, Вам {age} лет, Ваш знак зодиака {zodiacSign} и вы работаете {profession}.");
+        }
+    }
+}
