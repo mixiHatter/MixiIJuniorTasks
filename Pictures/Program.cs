4498d866a1df8d4371b2e3e98950223068ff450c
mixiHatter <5281cmixi@gmail.com>
2024-10-15 20:07:54 +0300

    addTaskPictures
    Completed

---
+++ Pictures/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pictures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int picturesRow = 3;
+            int userPictures = 52;
+            int fillRow;
+            int overfillRow;
+
+            fillRow = userPictures / picturesRow;
+            overfillRow = userPictures % picturesRow;
+
+            Console.WriteLine($"В ряду {picturesRow} картинок, у пользователя {userPictures} картинок.");
+            Console.WriteLine($"Заполненных рядов: {fillRow}, картинок сверх меры: {overfillRow}.");
+        }
+    }
+}
