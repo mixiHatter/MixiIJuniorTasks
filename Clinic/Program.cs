35e5fc6c5724bb2284832138ca952b163c7edcbc
mixiHatter <5281cmixi@gmail.com>
2024-10-15 23:26:38 +0300

    AddedNewTaskPoliclinic
    completed

---
+++ Clinic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace Clinic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countQueue;
+            int queueTime;
+            int consultationTimeMinutes = 10;
+            int minInHour = 60;
+            int hour;
+            int minutes;
+
+            Console.Write("Сколько людей в очереди:");
+            countQueue = Convert.ToInt32(Console.ReadLine());
+            queueTime = countQueue * consultationTimeMinutes;
+            hour = queueTime / minInHour;
+            minutes = queueTime % minInHour;
+            Console.WriteLine($"В очереди нужно отстоять: {hour} часов, {minutes} минут.");
+        }
+    }
+}
