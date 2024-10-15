2b751d96f17bd004f3a419d68087c1e9ea7efe49
mixiHatter <5281cmixi@gmail.com>
2024-10-15 20:25:43 +0300

    AddedNewTaskSwappingValues
    Competed

---
+++ SwappingValues/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwappingValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Васильев";
+            string surname = "Вася";
+            string buffer;
+            Console.WriteLine($"Имя: {name}, фамилия: {surname}.");
+            buffer = name;
+            name = surname;
+            surname = buffer;
+            Console.WriteLine($"Имя: {name}, фамилия: {surname}.");
+
+            string firstCup = "кофе";
+            string secondCup = "чай";
+            Console.WriteLine($"В первой чашке: {firstCup}, а во второй: {secondCup}");
+            buffer = firstCup;
+            firstCup = secondCup;
+            secondCup = buffer;
+            Console.WriteLine($"В первой чашке: {firstCup}, а во второй: {secondCup}");
+        }
+    }
+}
