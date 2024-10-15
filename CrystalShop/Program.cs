07bc6978134aa100bbd99f085de90a796b182256
mixiHatter <5281cmixi@gmail.com>
2024-10-15 20:37:50 +0300

    AddedNewTaskCrystalShop
    Completed

---
+++ CrystalShop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrystalShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int userCoins;
+            int crystalPrice = 12;
+            int userCrystals;
+            Console.Write("Сколько у вас монет: ");
+            userCoins = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Здравствуй путник! Цена кристалла {crystalPrice} монет.");
+            Console.Write("Сколько кристаллов возьмёшь? ");
+            userCrystals = Convert.ToInt32(Console.ReadLine());
+            userCoins = userCoins - crystalPrice * userCrystals;
+            Console.WriteLine($"У тебя {userCoins} монет и {userCrystals} кристалов");
+        }
+    }
+}
