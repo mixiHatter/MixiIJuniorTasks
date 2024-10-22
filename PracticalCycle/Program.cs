8cf44f0750e4cc6a3d7ed56e532a56c34ce4df9d
mixiHatter <5281cmixi@gmail.com>
2024-10-22 20:43:43 +0300

    AddedNewTaskPracticalCycle
    completed

---
+++ PracticalCycle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PracticalCycle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userText;
+            int userCountRepeats;
+
+            Console.Write("Вы хотите сказать: ");
+            userText = Convert.ToString(Console.ReadLine());
+            Console.Write("Сколько раз вы хотите это сказать: ");
+            userCountRepeats = Convert.ToInt32(Console.ReadLine());
+
+            while (userCountRepeats > 0) 
+            {
+                Console.WriteLine(userText);
+                userCountRepeats--;
+            }
+        }
+    }
+}
