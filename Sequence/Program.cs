c7a5830013101c2546f049f8bd3aae60e8790641
mixiHatter <5281cmixi@gmail.com>
2024-10-22 21:46:37 +0300

    AddNewTaskSequence
    completed

---
+++ Sequence/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ints = new List<int>() 
+            {5, 12, 19, 26, 33, 40, 47, 54, 61, 68, 75, 82, 89, 96, 103};
+            int count = 0;
+            int listCell = 0;
+            int maxListCell = ints.Count;
+
+            while (listCell < maxListCell) // не смог реализовать данную задачу через цикл for, по этому использовал while
+            {
+                if (count == ints[listCell])
+                {
+                    Console.WriteLine(count);
+                    listCell++;
+                }
+                else { count++; }
+            } 
+        }
+
+    }
+}
