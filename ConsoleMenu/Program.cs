74556c6ef8be86ab3a78b890a22acaf9ac575553
mixiHatter <5281cmixi@gmail.com>
2024-10-23 19:05:12 +0300

    AddTaskConsoleMenu
    complete

---
+++ ConsoleMenu/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ConsoleMenu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWork = true;
+            const string CommandEchoText = "1";
+            const string CommandSecretText = "2";
+            const string CommandRandomNumber = "3";
+            const string CommandClearConsole = "4";
+            const string CommandExitProgramm = "5";
+            string userText;
+            string userTextEcho;
+            Random random = new Random();
+            int randomNumber;
+
+            while (isWork)
+            {
+                Console.WriteLine($"Для дублирования вашего текста введите - {CommandEchoText}" +
+                    $"\nДля вывода секретиков введите - {CommandSecretText}" +
+                    $"\nДля вывода случайного числа введите - {CommandRandomNumber}" +
+                    $"\nДля очистки консоли введите - {CommandClearConsole}" +
+                    $"\nДля выхода из программы введите - {CommandExitProgramm}");
+
+                Console.Write("Введите команду: ");
+                userText = Convert.ToString(Console.ReadLine());
+
+                switch (userText)
+                {
+                    case CommandEchoText:
+                        Console.Write("Введите текст для дублирования:");
+                        userTextEcho = Console.ReadLine();
+                        Console.WriteLine(userTextEcho);
+                        break;
+                    case CommandSecretText:
+                        Console.Clear();
+                        Console.WriteLine("\"Компилятору все равно, как написан код. \r\nЕму не нужна мудрость в глазах кодера, чтобы всё это понять.\"\r\n@Почти В.Цой");
+                        Console.ReadKey();
+                        break;
+                    case CommandRandomNumber:
+                        randomNumber = random.Next(1, 101);
+                        Console.WriteLine(randomNumber);
+                        break;
+                    case CommandClearConsole:
+                        Console.Clear();
+                        break;
+                    case CommandExitProgramm:
+                        isWork = false;
+                        break;
+                    default:
+                        Console.WriteLine("Команда не распознана.");
+                        break;
+                }
+            }
+        }
+    }
+}
