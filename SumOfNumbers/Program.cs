30cc970d1828d8b6173e4e9d1a921186214bacc6
mixiHatter <5281cmixi@gmail.com>
2024-10-23 13:01:32 +0300

    AddNewTaskSumOfNumbers
    complete

---
+++ SumOfNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SumOfNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int number = rand.Next(0, 101);
+            int sumNumbers = 0;
+
+            Console.WriteLine(number);
+            for (int i = 0; i <= number; i++) 
+            {
+                if(i % 3 == 0 || i % 5 == 0) 
+                {
+                    sumNumbers+=i;
+                    Console.WriteLine(sumNumbers);
+                }
+            }
+        }
+    }
+}
