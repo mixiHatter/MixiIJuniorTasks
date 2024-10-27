ac6c6841a239613049ab720f1ddb9291de30ef9f
mixiHatter <5281cmixi@gmail.com>
2024-10-27 16:54:39 +0300

    AddedNewTaskNameOutput
    complete

---
+++ NameOutput/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NameOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string symbol;
+            string name;
+
+            Console.Write("Введите желаемый символ: ");
+            symbol = Console.ReadLine();
+            Console.Write("Введите имя: ");
+            name = Console.ReadLine();
+
+            for(int i = 0; i < name.Length + 2; i++)
+            {
+                Console.Write(symbol);
+            }
+
+            Console.WriteLine($"\n{symbol}{name}{symbol}");
+
+            for (int i = 0; i < name.Length + 2; i++)
+            {
+                Console.Write(symbol);
+            }
+        }
+    }
+}
