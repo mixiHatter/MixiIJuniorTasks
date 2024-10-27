0534baac30f0fe55e2b1478571596c2c5fe935ca
mixiHatter <5281cmixi@gmail.com>
2024-10-27 18:49:52 +0300

    AddedProgramUnderPassword
    complete

---
+++ ProgramUnderPassword/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ProgramUnderPassword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWork = true;
+            string password = "qwerty123";
+            string secretText = "U are the best";
+            string userText;
+            int tryCount = 3;
+
+            while (isWork)
+            {
+                Console.Write("Введите пароль: ");
+                userText = Console.ReadLine();
+
+                if(userText == password)
+                {
+                    Console.WriteLine(secretText);
+                    break;
+                }
+                else if (tryCount <= 1)
+                {
+                    isWork = false;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Пароль неверный!");
+                    Console.WriteLine($"У вас осталось {tryCount - 1} попыток");
+                    tryCount--;
+                }
+            }
+        }
+    }
+}
