060dff9a768a1430e27e29b6fa49465c1ce8d2c3
mixiHatter <5281cmixi@gmail.com>
2024-11-11 05:11:43 +0300

    AddedNewTask
    Complete.

---
+++ DegreeTwo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DegreeTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = random.Next(1, 150);
+            int numberDegree = 2;
+            int degree = 1;
+
+            Console.WriteLine($"Random dropped number: {number}");
+
+            while(numberDegree < number)
+            {
+                numberDegree += numberDegree;
+                degree++;
+            }
+            Console.WriteLine($"{number} > {numberDegree}, which is 2 to the power of {degree}");
+        }
+    }
+}
