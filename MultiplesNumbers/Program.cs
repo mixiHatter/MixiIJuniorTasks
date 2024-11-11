ca583e5629d340496e1745097fefa3c7340ef876
mixiHatter <5281cmixi@gmail.com>
2024-11-11 04:54:44 +0300

    AddedNewTask
    Done.

---
+++ MultiplesNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MultiplesNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = random.Next(10, 26);
+            int minRange = 50;
+            int maxRange = 150;
+            int countMultiples = 0;
+
+            Console.WriteLine($"Random droped number: {number}.");
+
+            for (int i = number; i <= maxRange; i += number)
+            {
+                if(i >= minRange)
+                {
+                    countMultiples++;
+                }
+            }
+
+            Console.WriteLine($"{countMultiples} multiples of {number} in the range from {minRange} to {maxRange}.");
+            Console.ReadKey();
+        }
+    }
+}
