f9f8b902338768bb7438056952c20e36eb6658df
mixiHatter <5281cmixi@gmail.com>
2024-11-18 22:33:24 +0300

    LargestElement
    complete

---
+++ LargestElement/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace LargestElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numbers = new int[10, 10];
+            Random random = new Random();
+            int randomMin = 1;
+            int randomMax = 101;
+            int largestElement = 0;
+
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    numbers[i, j] = random.Next(randomMin, randomMax);
+                    Console.Write(numbers[i, j]);
+                    Console.Write(",");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    if(largestElement < numbers[i, j])
+                    {
+                        largestElement = numbers[i, j];
+                    }
+                }
+            }
+
+            Console.WriteLine($"Laragest element: {largestElement}");
+
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    if(largestElement == numbers[i,j])
+                    {
+                        numbers[i, j] = 0;
+                    }
+                    Console.Write(numbers[i, j]);
+                    Console.Write(",");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
