ffdd9527b6f3307f69d4e62c64e2ad180bc178bd
mixiHatter <5281cmixi@gmail.com>
2024-11-24 20:59:15 +0300

    LocalMaximum
    Added.

---
+++ LocalMaximum/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace LocalMaximum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxRange = 100;
+            int[] array = new int[30];
+
+            GetArray(ref array, array.Length, maxRange);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i == 0)
+                {
+                    if (array[i] > array[i + 1])                    
+                        Console.WriteLine($"{array[i]} > {array[i + 1]}");
+                    else
+                        Console.WriteLine($"{array[i]} < {array[i + 1]}");
+                }
+
+                else if (i == array.Length - 1)
+                {
+                    if (array[i] > array[i - 1] )
+                        Console.WriteLine($"{array[i]} > {array[i - 1]}");
+                    else
+                        Console.WriteLine($"{array[i]} < {array[i - 1]}");
+                }
+
+                else if (array[i] > array[i - 1] && array[i] > array[i + 1])
+                {
+                    Console.WriteLine($"{array[i - 1]} < {array[i]} > {array[i + 1]}");
+                }
+            }
+
+        }
+
+        static int[] GetArray(ref int[] array, int lengthArray, int maxRange, int minRange = 0)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < lengthArray; i++)
+                array[i] = random.Next(minRange, maxRange++);
+
+            return array;
+        }
+    }
+}
