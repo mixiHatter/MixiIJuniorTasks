5d8abd147e6856bc91ae13a73259d4a4478c2d88
mixiHatter <5281cmixi@gmail.com>
2024-11-26 22:54:21 +0300

    ShiftingArrayValues
    Added

---
+++ ShiftingArrayValues/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ShiftingArrayValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            Random random = new Random();
+            int minRange = 0;
+            int maxRange = 10;
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int bufer = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+                numbers[i] = random.Next(minRange, maxRange + 1);
+
+            if (userInput > 0)
+            {
+                for (int i = 0; i < numbers.Length - userInput; i++)
+                {
+                    bufer = numbers[i];
+                    numbers[i] = numbers[i + 1];
+                    numbers[i + 1] = bufer;
+                }
+            }
+
+            foreach (int number in numbers)
+                Console.Write(number + ",");
+        }
+    }
+}
