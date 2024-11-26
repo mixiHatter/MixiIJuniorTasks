3369e09ea7569d6df59229e0ad29e1fa8dd39d5f
mixiHatter <5281cmixi@gmail.com>
2024-11-26 18:20:36 +0300

    SortingNumbers
    added.

--- SortingNumbers/Program.cs
+++ SortingNumbers/Program.cs
@@ -10,14 +10,26 @@ namespace SortingNumbers
             Random random = new Random();
             int minRange = 0;
             int maxRange = 10;
+            int clipboard = 0;
 
             for (int i = 0; i < numbers.Length; i++)
                 numbers[i] = random.Next(minRange, maxRange + 1);
 
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers.Length - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        clipboard = numbers[j + 1];
+                        numbers[j + 1] = numbers[j];
+                        numbers[j] = clipboard;
+                    }
+                }
+            }
+
             foreach (int number in numbers)
                 Console.Write(number + ",");
-
-
         }
     }
 }
