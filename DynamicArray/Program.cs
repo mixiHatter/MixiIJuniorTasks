8dd47ceddbe8f8798e2ee45167dd802a68dac809
mixiHatter <5281cmixi@gmail.com>
2024-11-26 16:48:51 +0300

    DynamicArray
    Added.

---
+++ DynamicArray/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DynamicArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWork = true;
+            string userInput;
+            int[] userNumbers = new int[0];
+            int sum = 0;
+
+            while (isWork)
+            {
+                foreach (int i in userNumbers)
+                    Console.Write(i + ",");
+
+                Console.WriteLine();
+                userInput = Console.ReadLine();
+
+                if (userInput == "sum")
+                {
+                    foreach(int i in userNumbers)
+                        sum += i;
+
+                    Console.WriteLine(sum);
+                    Console.ReadKey();
+                }
+
+                else if (userInput == "exit") 
+                    isWork = false;
+
+                else
+                {
+                    int[] arrayExtension = new int[userNumbers.Length + 1];
+
+                    for(int i = 0;i < userNumbers.Length; i++)
+                    {
+                        arrayExtension[i] = userNumbers[i];
+                    }
+
+                    arrayExtension[userNumbers.Length] = Convert.ToInt32(userInput);
+                    userNumbers = arrayExtension;
+                }
+
+                Console.Clear();
+            }
+        }
+    }
+}
