6570fcd9822c9cb07ff7b202dde2dfabd93685aa
mixiHatter <5281cmixi@gmail.com>
2024-11-26 22:33:15 +0300

    Split
    Added

---
+++ Split/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Split
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Никогда не ошибается тот, кто ничего не делает";
+            string[] words = text.Split(' ');
+
+            foreach (string word in words)
+                Console.WriteLine(word);
+        }
+    }
+}
