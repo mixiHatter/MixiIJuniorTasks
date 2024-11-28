2f3df9c44044a067d54abbd3b76579cff9192c84
mixiHatter <5281cmixi@gmail.com>
2024-11-28 22:52:26 +0300

    ShiftingArrayValues
    Added

---
+++ ParenthesisExpression/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ParenthesisExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine(); ;
+            int openingBrecketCount = 0;
+            int closingBrecketCount = 0;
+            char openingBrecket = '(';
+            char closingBrecket = ')';
+            int maxDepth = 0;
+            int depth = 0;
+            bool isCorrect = true;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == openingBrecket)
+                {
+                    openingBrecketCount++;
+                    depth++;
+
+                    if (depth > maxDepth)
+                        maxDepth = depth;
+                }
+                else if (text[i] == closingBrecket)
+                {
+                    closingBrecketCount++;
+                    depth--;
+
+                    if (depth < 0)
+                        isCorrect = false;
+                }
+            }
+
+            if (depth == 0 && isCorrect)
+                Console.WriteLine($"{text} - строка корректная и максимум глубины равняется {maxDepth}.");
+            else
+                Console.WriteLine($"{text} - строка некорректная.");
+        }
+    }
+}
--- ShiftingArrayValues/Program.cs
+++ ShiftingArrayValues/Program.cs
@@ -6,28 +6,27 @@ namespace ShiftingArrayValues
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[10];
+            int[] numbers = new int[5];
             Random random = new Random();
             int minRange = 0;
             int maxRange = 10;
-            int userInput = Convert.ToInt32(Console.ReadLine());
+            int userInput;
             int bufer = 0;
 
             for (int i = 0; i < numbers.Length; i++)
                 numbers[i] = random.Next(minRange, maxRange + 1);
 
-            if (userInput > 0)
+            userInput = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0;i < userInput;i++)
             {
-                for (int i = 0; i < numbers.Length - userInput; i++)
+                for(int j = 0; j < numbers.Length - 1;j++)
                 {
-                    bufer = numbers[i];
-                    numbers[i] = numbers[i + 1];
-                    numbers[i + 1] = bufer;
+                    bufer = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = bufer;
                 }
             }
-
-            foreach (int number in numbers)
-                Console.Write(number + ",");
         }
     }
 }
