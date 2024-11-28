3f75efc133366a553dc5f7117b17a2dfb2e2f426
mixiHatter <5281cmixi@gmail.com>
2024-11-28 23:47:12 +0300

    ReadInt
    Added

---
+++ ReadInt/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ReadInt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isConvertCorrect = false;
+
+            while (isConvertCorrect == false)
+            {
+                isConvertCorrect = ReadInt(Console.ReadLine(), out int number);
+
+                if (isConvertCorrect == true)
+                    Console.WriteLine($"Number: {number}");
+                else
+                    Console.WriteLine("Repeat the input");
+            }
+        }
+
+        static bool ReadInt(string text, out int number)
+        {
+            bool result = int.TryParse(text, out var convertNumber);
+            number = convertNumber;
+
+            return result;
+        }
+    }
+}
