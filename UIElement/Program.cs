3d9cdf4e279a3f39ff8788b73ccfb48f74745867
mixiHatter <5281cmixi@gmail.com>
2024-11-29 20:54:46 +0300

    UIElement
    update

--- UIElement/Program.cs
+++ UIElement/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace UIElement
 {
@@ -8,53 +9,76 @@ namespace UIElement
         {
             int mana = 0;
             int healthPoint = 0;
+            int positionXHealthPoint = 30;
             int positionXMana = 60;
             int sizeBar;
 
             Console.WriteLine("Введите кол-во жизни: ");
-            healthPoint = Convert.ToInt32(Console.ReadLine());
+            healthPoint = ReadInt((Console.ReadLine()));
             Console.WriteLine("Введите длину бара: ");
-            sizeBar = Convert.ToInt32(Console.ReadLine());
+            sizeBar = ReadInt((Console.ReadLine()));
             Console.Clear();
 
-            GetDrawBar(healthPoint, sizeBar);
+            Console.SetCursorPosition(positionXHealthPoint, 0);
+            Console.WriteLine(GetDrawBar(healthPoint, sizeBar));
 
             Console.WriteLine();
             Console.WriteLine("Введите кол-во маны: ");
-            mana = Convert.ToInt32(Console.ReadLine());
+            mana = ReadInt((Console.ReadLine()));
             Console.WriteLine("Введите длину бара: ");
-            sizeBar = Convert.ToInt32(Console.ReadLine());
+            sizeBar = ReadInt((Console.ReadLine()));
 
-            GetDrawBar(mana, sizeBar, positionXMana);
+            Console.SetCursorPosition(positionXMana, 0);
+            Console.WriteLine(GetDrawBar(mana, sizeBar));
+            
 
             Console.ReadKey();
         }
 
-        static void GetDrawBar(int fill, int sizeBar, int positionX = 30, int positionY = 0)
+        static int ReadInt(string text)
         {
+            bool result = int.TryParse(text, out var convertNumber);
+
+            while (result == false)
+            {
+                Console.WriteLine("Введите повторно:");
+
+                result = int.TryParse(Console.ReadLine(), out var number);
+                convertNumber = number;
+            }
+
+            return convertNumber;
+        }
+        static string DrawBar(char symbol, double length)
+        {
+            string text = string.Empty;
+            for (int i = 1; i <= length; i++)
+                text += symbol;
+
+            return text;
+        }
+
+        static string GetDrawBar(int fill, int sizeBar)
+        {
+            string text = string.Empty;
             char havePoint = '#';
             char freePont = '_';
+            char openingBracket = '[';
+            char closingBracket = ']';
             double maxProcent = 100;
-            double oneProcentBar = Convert.ToDouble(sizeBar) / maxProcent;
+            double oneProcentOfSizeBar = Convert.ToDouble(sizeBar) / maxProcent;
 
             if (fill > maxProcent)
                 fill = Convert.ToInt32(maxProcent);
+            else if (fill < 0)
+                fill = 0;
 
-            int fillProcent = Convert.ToInt32(Convert.ToDouble(fill) * oneProcentBar);
+            int fillProcent = Convert.ToInt32(Convert.ToDouble(fill) * oneProcentOfSizeBar);
             int freeBar = Convert.ToInt32(Convert.ToDouble(sizeBar) - fillProcent);
 
-            char DrawBar(char symbol, double length)
-            {
-                for (int i = 1; i <= length; i++)
-                    Console.Write(symbol);
-                return symbol;
-            }
+            text += openingBracket + DrawBar(havePoint, fillProcent) + DrawBar(freePont, freeBar) + closingBracket;
 
-            Console.SetCursorPosition(positionX, positionY);
-            Console.Write("[");
-            DrawBar(havePoint, fillProcent);
-            DrawBar(freePont, freeBar);
-            Console.Write("]");
+            return text;
         }
     }
 }
