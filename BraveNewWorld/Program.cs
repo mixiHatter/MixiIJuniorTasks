c8a6cd8d7e249cde4afa1aa8c2c28d5b2e991292
mixiHatter <5281cmixi@gmail.com>
2025-02-16 16:35:41 +0300

    ready to first check

--- BraveNewWorld/Program.cs
+++ BraveNewWorld/Program.cs
@@ -7,28 +7,45 @@ namespace BraveNewWorld
         static void Main(string[] args)
         {
             char[,] map = new char[,] {
-                                      { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
-                                      { '#', ' ', '#', ' ', ' ', ' ', '#', ' ', ' ', '#'},
-                                      { '#', ' ', '#', ' ', '#', ' ', '#', ' ', ' ', '#'},
-                                      { '#', ' ', '#', ' ', '#', ' ', '#', ' ', ' ', '#'},
-                                      { '#', ' ', '#', ' ', '#', ' ', '#', ' ', ' ', '#'},
-                                      { '#', ' ', '#', ' ', '#', ' ', '#', ' ', ' ', '#'},
-                                      { '#', ' ', '#', ' ', '#', ' ', '#', ' ', ' ', '#'},
-                                      { '#', ' ', '#', ' ', '#', ' ', '#', '#', ' ', '#'},
-                                      { '#', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', '#'},
-                                      { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}
-                                      };
+                                      { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+                                      { '#', ' ', '#', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#'},
+                                      { '#', ' ', '#', ' ', '#', ' ', '#', ' ', '#', ' ', '#'},
+                                      { '#', ' ', '#', ' ', '#', ' ', '#', ' ', '#', ' ', '#'},
+                                      { '#', ' ', '#', ' ', '#', ' ', '#', ' ', '#', ' ', '#'},
+                                      { '#', ' ', '#', ' ', '#', ' ', '#', ' ', '#', ' ', '#'},
+                                      { '#', ' ', '#', ' ', '#', ' ', '#', ' ', '#', ' ', '#'},
+                                      { '#', ' ', '#', ' ', '#', ' ', '#', ' ', '#', ' ', '#'},
+                                      { '#', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#', ' ', '#'},
+                                      { '#', ' ', '#', ' ', '#', ' ', '#', ' ', '#', ' ', '#'},
+                                      { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}
+                                      }; 
+            char[,] mapActive = new char[map.GetLength(0), map.GetLength(1)];
+            int mapSize;
             char player = '@';
             char wall = '#';
+            int[] playerPosition;
 
-            SpawnPlayer(map, player, wall);
+            ConsoleKeyInfo keyUp = new ConsoleKeyInfo('w', ConsoleKey.W, false, false, false);
+            ConsoleKeyInfo keyDown = new ConsoleKeyInfo('s', ConsoleKey.S, false, false, false);
+            ConsoleKeyInfo keyLeft = new ConsoleKeyInfo('a', ConsoleKey.A, false, false, false);
+            ConsoleKeyInfo keyRight = new ConsoleKeyInfo('d', ConsoleKey.D, false, false, false);
+
+            mapSize = map.Length;
+            playerPosition = GetPositionToSpawnPlayer(map, wall);
+            Array.Copy(map, mapActive, mapSize);
 
             while (true)
             {
                 Console.Clear();
-                ShowMap(map);
 
-                Console.ReadKey();
+                ShowPlayer(mapActive, player, playerPosition);
+                ShowMap(mapActive);
+
+                ConsoleKeyInfo playerKey = Console.ReadKey();
+
+                playerPosition = PlayerMove(mapActive, wall, playerPosition, playerKey, keyUp, keyDown, keyLeft, keyRight);
+
+                Array.Copy(map, mapActive, mapSize);
             }
         }
 
@@ -44,23 +61,92 @@ namespace BraveNewWorld
             }
         }
 
-        static char[,] SpawnPlayer(char[,] map, char player, char wall)
+        static int[] GetPositionToSpawnPlayer(char[,] mapActive, char wall)
         {
-            bool isVisiblePlayer = false;
+            bool playerIsSpawn = false;
+            int[] playerPosition = new int[2];
 
-            for (int i = 0; i < map.GetLength(1); i++)
+            for (int i = 0; i < mapActive.GetLength(1); i++)
             {
-                for (int j = 0; j < map.GetLength(0); j++)
+                for (int j = 0; j < mapActive.GetLength(0); j++)
                 {
-                    if (map[i,j] != wall && isVisiblePlayer !=true)
+                    if (mapActive[i,j] != wall && playerIsSpawn == false)
                     {
-                        map[i,j] = player;
-                        isVisiblePlayer = true;
+                        playerPosition[0] = i;
+                        playerPosition[1] = j;
+
+                        playerIsSpawn = true;
                     }
                 }
             }
 
-            return map;
+            return playerPosition;
         }
+
+        static void ShowPlayer(char[,] mapActive, char player, int[] playerPosition)
+        {
+            mapActive[playerPosition[0],playerPosition[1]] = player;
+        }
+
+        static int[] PlayerMove(char[,] mapActive, char wall, int[] playerPosition, ConsoleKeyInfo playerKey, ConsoleKeyInfo keyUp, ConsoleKeyInfo keyDown, ConsoleKeyInfo keyRight, ConsoleKeyInfo keyLeft)
+        {
+            bool barrierIsAbsent;
+            if (playerKey == keyUp) 
+            {
+                playerPosition[0] = playerPosition[0] - 1;
+
+                barrierIsAbsent = WallCheck(mapActive, wall, playerPosition);
+
+                if (barrierIsAbsent == false)
+                {
+                    playerPosition[0] = playerPosition[0] + 1;
+                }
+            }
+            else if (playerKey == keyDown)
+            {
+                playerPosition[0] = playerPosition[0] + 1;
+
+                barrierIsAbsent = WallCheck(mapActive, wall, playerPosition);
+
+                if (barrierIsAbsent == false)
+                {
+                    playerPosition[0] = playerPosition[0] - 1;
+                }
+            }
+            else if (playerKey == keyLeft)
+            {
+                playerPosition[1] = playerPosition[1] + 1;
+
+                barrierIsAbsent = WallCheck(mapActive, wall, playerPosition);
+
+                if (barrierIsAbsent == false)
+                {
+                    playerPosition[1] = playerPosition[1] - 1;
+                }
+            }
+            else if (playerKey == keyRight)
+            {
+                playerPosition[1] = playerPosition[1] - 1;
+
+                barrierIsAbsent = WallCheck(mapActive, wall, playerPosition);
+
+                if (barrierIsAbsent == false)
+                {
+                    playerPosition[1] = playerPosition[1] + 1;
+                }
+            }
+
+            return playerPosition;
+        }
+
+        static bool WallCheck (char[,] mapActive, char wall, int[] playerPosition)
+        {
+            if (mapActive[playerPosition[0], playerPosition[1]] == wall)
+            {
+                return false;
+            }
+            else return true;
+        }
+        
     }
 }
